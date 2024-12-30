b9a4e7e8eaaabc095cbe14ed376ddd755aaa3879
Roma <rcemeniuk@gmail.com>
2024-12-30 13:48:03 +0100

    fix

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import pygame
 import logging
 from copy import deepcopy as c
 
+pygame.init()
+
 logging.basicConfig(
-    filename="tictactoe.log",
-    filemode="w",
+    # filename="tictactoe.log",
+    # filemode="w",
     level=logging.DEBUG,
     format="%(levelname)s - %(message)s",
 )
@@ -45,6 +47,7 @@ class Board:
             if self.___board[0][i] == self.___board[1][i] == self.___board[2][i] != " ":
                 return self.___board[0][i]
         if self.___board[0][0] == self.___board[1][1] == self.___board[2][2] != " ":
+
             return self.___board[0][0]
         if self.___board[0][2] == self.___board[1][1] == self.___board[2][0] != " ":
             return self.___board[0][2]
@@ -66,9 +69,11 @@ class Board:
         """
         if not self.is_available(pos):
             raise ValueError(f"Position {pos} is not available")
+
         logging.info("Adding %s to %s", move.value, pos)
         x, y = pos
         self.___board[y][x] = move
+
         return self
 
     def __str__(self) -> str:
@@ -93,7 +98,7 @@ class TicTacToe(pygame.sprite.Sprite):
     board_size: int = 300
 
     image: pygame.Surface = pygame.Surface((board_size, board_size))
-    rect: pygame.Rect = image.get_rect()
+    rect: pygame.Rect = image.get_rect(topleft=(10, 10))
     move_size: int = 81
     board_texture: pygame.Surface = scale(
         load("./img/tic-tac-toe.png"), (board_size, board_size)
@@ -106,7 +111,7 @@ class TicTacToe(pygame.sprite.Sprite):
     )
 
     def update(self):
-        self.image.fill((255, 255, 255))
+        self.image.fill((100, 100, 100))
         self.image.blit(self.board_texture, (0, 0))
         offset = 9
         for y, row in enumerate(self.board.board):
@@ -123,7 +128,7 @@ class TicTacToe(pygame.sprite.Sprite):
 
 def main():
 
-    screen = pygame.display.set_mode((300, 300))
+    screen = pygame.display.set_mode((320, 320))
     pygame.display.set_caption("Tic Tac Toe")
     clock = pygame.time.Clock()
     running = True
@@ -141,21 +146,56 @@ def main():
                 y //= 100
                 try:
                     ttt.board.make_move(state, (x, y))
+                    if not ttt.board.check_winner() and not ttt.board.is_full():
+                        pygame.mixer.Sound("./sound/insert.wav").play()
                     state = Move.CROSS if state == Move.CIRCLE else Move.CIRCLE
                 except ValueError as e:
                     logging.error(e)
+                except IndexError:
+                    logging.error("User clicked outside the board")
 
         tictactoe.update()
-        if winner := ttt.board.check_winner():
-            logging.info("Winner is %s", winner.value)
-            running = False
-        if ttt.board.is_full():
-            logging.info("It's a tie")
-            running = False
+        screen.fill((100, 100, 100))
 
         tictactoe.draw(screen)
 
         pygame.display.flip()
+
+        if winner := ttt.board.check_winner():
+            board = ttt.board.board
+            line_color = (0, 200, 0)
+            line_width = 9
+            for i in range(3):
+                if board[i][0] == board[i][1] == board[i][2] != " ":
+                    pygame.draw.line(
+                        screen,
+                        line_color,
+                        (0 + 10, i * 100 + 50 + 10),
+                        (300 + 10, i * 100 + 50 + 10),
+                        line_width,
+                    )
+                if board[0][i] == board[1][i] == board[2][i] != " ":
+                    pygame.draw.line(
+                        screen,
+                        line_color,
+                        (i * 100 + 50 + 10, 0 + 10),
+                        (i * 100 + 50 + 10, 300 + 10),
+                        line_width,
+                    )
+            if board[0][0] == board[1][1] == board[2][2] != " ":
+                pygame.draw.line(screen, line_color, (10, 10), (310, 310), line_width)
+            if board[0][2] == board[1][1] == board[2][0] != " ":
+                pygame.draw.line(screen, line_color, (310, 10), (10, 310), line_width)
+            pygame.display.flip()
+            pygame.mixer.Sound("./sound/game-over.wav").play()
+            pygame.time.wait(2500)
+            logging.info("Winner is %s", winner.value)
+            running = False
+        if ttt.board.is_full():
+            pygame.mixer.Sound("./sound/game-over.wav").play()
+            pygame.time.wait(2500)
+            logging.info("It's a tie")
+            running = False
         clock.tick(60)
 
 
